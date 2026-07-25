b21e11c27c217e979064adc4bc5d07bb6cfd682e
Shravani Shinde <shinde.shravani39@gmail.com>
2026-07-25 23:57:50 +0530

    Implement main method to read and print inputs

---
+++ Java-PrintStatement/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int A= sc.nextInt();
+       double B=sc.nextDouble();
+       sc.nextLine();
+       String line=sc.nextLine();
+       System.out.println("String: "+line);
+       System.out.println("Double: "+B);
+       System.out.println("Int: "+A);
+    }
+}
